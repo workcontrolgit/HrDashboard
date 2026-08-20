0ecb2ca8791e8c3f9511c0a1e3c0f15d645daa2c
Fuji Nguyen <fuji.nguyen@workcontrol.com>
2026-08-19 20:28:24 -0400

    feat(web): add ChatSessionService and MessageViewModel for streaming conversation state

--- src/HrDashboard.Web/Services/_Placeholder.cs
+++
@@ -1,9 +0,0 @@
-namespace HrDashboard.Web.Services;
-
-/// <summary>
-/// Placeholder to enable namespace during Task 6.
-/// Actual services will be added in Task 13.
-/// </summary>
-public class _Placeholder
-{
-}
